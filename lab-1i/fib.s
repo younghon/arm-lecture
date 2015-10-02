c0b4a663e79b531dbaa3d1225b133a2213d2bfd4
younghon <younghonwill@hotmail.com>
2015-10-02 17:11:31 +0800

    add comment

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,15 +13,19 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, r6, lr}
 
-	@previous = -1
-	@result = 1
-	@sum = 0
+	@r3 previous = -1
+	@r4 result = 1
+	@r5 sum = 0
 	mov r3, #-1
 	mov r4, #1
 	mov r5, #0
 
+	@r6 for loop counter
 	mov r6, #0
 label:
+	@sum = previous + result
+	@previous = result
+	@result = sum
 	add r5, r3, r4
 	mov r3, r4
 	mov r4, r5
@@ -29,6 +33,7 @@ label:
 	cmp r6, r0
 	ble label
 
+	@r0 = r4  (return r0)
 	mov r0, r4
 	pop {r3, r4, r5, r6, pc}		@EPILOG
 
